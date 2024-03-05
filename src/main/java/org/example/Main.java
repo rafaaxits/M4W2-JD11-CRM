ebdddb3d36cd63f28531d8de53ba5401196253db
rafaaxits <rafaaxits@gmail.com>
2024-03-05 20:38:59 -0300

    modidying Main.java to handle Clients instantiations

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,12 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Cliente clienteM = new Cliente("000.111.222-33", "João Batista", 27,
+                "Rua teste git", 'M');
+        System.out.printf(clienteM.toString());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Cliente clienteF = new Cliente("444.555.666-77", "Maria Batista", 25,
+                "Rua teste git", 'F');
+        System.out.printf(clienteF.toString());
     }
 }
